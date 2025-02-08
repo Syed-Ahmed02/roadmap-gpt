c4bbf4dad1ef823ce6ca5af58b954e37bbc4c038
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2025-02-07 23:16:21 -0500

    updated avatars, and eslint rules

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['avatar.iran.liara.run'],
+  },
+  eslint:{
+    ignoreDuringBuilds:true
+  }
+
 };
 
 export default nextConfig;
--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -12,6 +12,8 @@ import {
 import { ChatMessageList } from "@/components/ui/chat-message-list";
 import { ChatInput } from "@/components/ui/chat-input";
 
+
+
 export function Chat({ formData }: { formData: z.infer<typeof FormSchema> }) {
     const [messages, setMessages] = useState([
     {
@@ -75,8 +77,8 @@ export function Chat({ formData }: { formData: z.infer<typeof FormSchema> }) {
                 className="h-8 w-8 shrink-0"
                 src={
                   message.sender === "user"
-                    ? "https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=64&h=64&q=80&crop=faces&fit=crop"
-                    : "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=64&h=64&q=80&crop=faces&fit=crop"
+                    ? `https://avatar.iran.liara.run/public/30`
+                    : `https://avatar.iran.liara.run/public/10`
                 }
                 fallback={message.sender === "user" ? "US" : "AI"}
               />
