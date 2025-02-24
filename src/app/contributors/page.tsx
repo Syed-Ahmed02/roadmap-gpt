0f477e378ca10a590cc03174119ecdc21ddc6cd2
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2025-02-23 20:12:07 -0500

    updated ui and added contributors page

---
+++ src/app/contributors/page.tsx
@@ -0,0 +1,58 @@
+import { BlurFade } from "@/components/ui/blur-fade"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { Separator } from "@/components/ui/separator"
+import Link from "next/link"
+export default function Contributors() {
+
+    const contributorsList = [
+        'https://github.com/codecrafters-io/build-your-own-x', 'https://github.com/practical-tutorials/project-based-learning',
+        'https://github.com/josephmisiti/awesome-machine-learning', 'https://github.com/anthropics/courses',
+        'https://github.com/lorinpop17/app-ideas', 'https://github.com/charlax/professional-programming',
+        'https://github.com/aishwaryanr/awesome-generative-ai-guide', 'https://github.com/liuchong/awesome-roadmaps',
+        'https://github.com/detailyang/awesome-cheatsheet', 'https://github.com/cloudcommunity/Free-Certifications',
+        'https://github.com/ChristosChristofidis/awesome-deep-learning', 'https://github.com/docker/awesome-compose',
+        'https://github.com/academic/awesome-datascience', 'https://github.com/freeCodeCamp/freeCodeCamp',
+        'https://github.com/EbookFoundation/free-programming-books', 'https://github.com/A-to-Z-Resources-for-Students',
+        'https://github.com/Hack-with-Github/Awesome-Hacking', 'https://github.com/prakhar1989/awesome-courses',
+        'https://github.com/trimstray/the-book-of-secret-knowledge', 'https://github.com/brexhq/prompt-engineering',
+        'https://github.com/mlabonne/llm-course', 'https://github.com/sindresorhus/awesome', 'https://github.com/MunGell/awesome-for-beginners',
+        'https://github.com/kamranahmedse/developer-roadmap', 'https://github.com/bradtraversy/50projects50days',
+        'https://github.com/mtdvio/every-programmer-should-know', 'https://github.com/shahednasser/awesome-resources',
+        'https://github.com/microsoft/Data-Science-For-Beginners', 'https://github.com/natnew/Awesome-Data-Science',
+        'https://github.com/goabstract/Marketing-for-Engineers']
+
+
+    return (
+        <div className="min-h-screen bg-primary">
+            <div className="flex flex-col items-center mx-8">
+                <div className="my-16 text-center  w-full text-primary-foreground space-y-4">
+                    <BlurFade delay={0.01} inView={true}>
+                        <h1 className="text-2xl md:text-6xl font-semibold max-w-5xl mx-auto">
+                            This was only possible due to the amazing resources on github.
+                        </h1>
+                    </BlurFade>
+
+                    <BlurFade delay={0.03} inView={true}>
+                        <p className="text-md md:text-xl w-full ">
+                            Here is the list of reposotires we used
+                        </p>
+                    </BlurFade>
+                </div>
+                <ScrollArea className="h-96 rounded-md border bg-secondary">
+                    <div className="p-4">
+                        <h4 className="mb-4 text-sm font-medium leading-none">Repos</h4>
+                        {contributorsList.map((tag) => (
+                            <div >
+                                <Link href={tag} key={tag} className="text-sm hover:bg-secondary-foreground hover:text-primary ease-linear duration-100 hover:border hover:rounded-md p-2 ">
+                                    {tag}
+                                </Link>
+                                <Separator className="my-2" />
+                            </div>
+                        ))}
+                    </div>
+                </ScrollArea>
+            </div>
+        </div>
+    )
+}
+
--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -8,22 +8,22 @@ export function Hero() {
 
             <div className="flex flex-col items-center mx-8">
                 <div className="my-16 text-center  w-full text-primary-foreground space-y-4">
-                    <BlurFade delay={0.05}>
+                    <BlurFade delay={0.01} inView={true}>
                         <h2 className="text-xl md:text-2xl  font-semibold w-full">
                             Learning a new skill has never been easier
                         </h2>
                     </BlurFade>
-                    <BlurFade delay={0.1}>
+                    <BlurFade delay={0.02} inView={true}>
                         <h1 className="text-2xl md:text-6xl font-semibold max-w-5xl mx-auto">
                             Master any skill with a <u className="font-bold">personalized</u> AI Generated Roadmap
                         </h1>
                     </BlurFade>
-                    <BlurFade delay={0.15}>
+                    <BlurFade delay={0.03} inView={true}>
                         <p className="text-md md:text-xl w-full ">
                             No more confusion. No more wasted time. Just a clear, structured path to mastering your next skill.
                         </p>
                     </BlurFade>
-                    <BlurFade delay={0.2}>
+                    <BlurFade delay={0.04} inView={true}>
                         <Link href="#form">
                             <Button variant={"outline"} className="font-bold mt-8">Get Your Custom Roadmap Now</Button>
                         </Link>
